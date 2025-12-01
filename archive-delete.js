0722f88194e11c34355a587fe1e819d8731ef410
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2025-12-01 17:48:36 +0500

    Update archive-delete.js

--- archive-delete.js
+++ archive-delete.js
@@ -1,35 +1,29 @@
-// ===============================================
-// 4) ARCHIVE DELETE  (DATE RANGE DELETE)
-// ===============================================
 app.post("/api/archive-delete", async (req, res) => {
   try {
     const { start_date, end_date, password } = req.body;
 
-    if (password !== "faizanyounus")
+    if (password !== "faizanyounus2122")
       return res.json({ success: false, error: "Wrong password" });
 
-    // DELETE PURCHASES
     await supabase
       .from("purchases")
       .delete()
       .gte("purchase_date", start_date)
       .lte("purchase_date", end_date);
 
-    // DELETE SALES
     await supabase
       .from("sales")
       .delete()
       .gte("sale_date", start_date)
       .lte("sale_date", end_date);
 
-    // DELETE RETURNS
     await supabase
       .from("sale_returns")
       .delete()
       .gte("created_at", start_date)
       .lte("created_at", end_date);
 
-    res.json({ success: true });
+    res.json({ success: true, message: "Deleted Successfully" });
 
   } catch (err) {
     res.json({ success: false, error: err.message });
